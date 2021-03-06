ea746bd1252c40c5f2a4f302b9b5736c6992bce1
hongyang-work <hong.yang.code@gmail.com>
2021-03-06 20:59:26 +0800

    removed redundant try catch

--- src/main/java/com/gov/dsta/coldstraw/controller/UserController.java
+++ src/main/java/com/gov/dsta/coldstraw/controller/UserController.java
@@ -2,20 +2,15 @@ package com.gov.dsta.coldstraw.controller;
 
 import com.gov.dsta.coldstraw.assembler.GroupAssembler;
 import com.gov.dsta.coldstraw.assembler.UserAssembler;
-import com.gov.dsta.coldstraw.exception.Group.GroupNotFoundException;
-import com.gov.dsta.coldstraw.exception.user.UserNotFoundException;
 import com.gov.dsta.coldstraw.model.Group;
 import com.gov.dsta.coldstraw.model.Notification;
 import com.gov.dsta.coldstraw.model.User;
-import com.gov.dsta.coldstraw.service.GroupService;
 import com.gov.dsta.coldstraw.service.UserService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,19 +21,19 @@ public class UserController {
 
     private final UserService userService;
     private final UserAssembler userAssembler;
-    private final GroupService groupService;
     private final GroupAssembler groupAssembler;
 
-    public UserController(UserService userService, UserAssembler userAssembler, GroupService groupService, GroupAssembler groupAssembler) {
+    public UserController(UserService userService, UserAssembler userAssembler, GroupAssembler groupAssembler) {
         this.userService = userService;
         this.userAssembler = userAssembler;
-        this.groupService = groupService;
         this.groupAssembler = groupAssembler;
     }
 
     @GetMapping()
     public CollectionModel<EntityModel<User>> getUsers() {
-        List<EntityModel<User>> users = userService.getUsers().stream()
+        List<EntityModel<User>> users = userService
+                .getUsers()
+                .stream()
                 .map(userAssembler::toModel)
                 .collect(Collectors.toList());
         return userAssembler.toCollectionModel(users);
@@ -46,17 +41,15 @@ public class UserController {
 
     @GetMapping("/{userId}")
     public EntityModel<User> getUser(@PathVariable Long userId) {
-        try {
-            User user = userService.getUser(userId);
-            return userAssembler.toModel(user);
-        } catch (UserNotFoundException ex) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
-        }
+        User user = userService.getUser(userId);
+        return userAssembler.toModel(user);
     }
 
     @GetMapping("/{userId}/groups")
     public CollectionModel<EntityModel<Group>> getGroups(@PathVariable Long userId) {
-        List<EntityModel<Group>> groups = userService.getUserGroups(userId).stream()
+        List<EntityModel<Group>> groups = userService
+                .getUserGroups(userId)
+                .stream()
                 .map(groupAssembler::toModel)
                 .collect(Collectors.toList());
         return groupAssembler.toCollectionModel(groups);
@@ -64,12 +57,8 @@ public class UserController {
 
     @GetMapping("/{userId}/groups/{groupId}")
     public EntityModel<Group> getGroup(@PathVariable Long userId, @PathVariable Long groupId) {
-        try {
-            Group group = userService.getUserGroup(userId, groupId);
-            return groupAssembler.toModel(group);
-        } catch(GroupNotFoundException ex) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage(), ex);
-        }
+        Group group = userService.getUserGroup(userId, groupId);
+        return groupAssembler.toModel(group);
     }
 
     @GetMapping("/{userId}/notifications/sent")
