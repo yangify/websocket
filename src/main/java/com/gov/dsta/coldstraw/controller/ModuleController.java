1538b851f456948b9388c372e5ebc55d78f0e124
hongyang-work <hong.yang.code@gmail.com>
2021-03-07 10:01:48 +0800

    updated ModuleController to return response code

--- src/main/java/com/gov/dsta/coldstraw/controller/ModuleController.java
+++ src/main/java/com/gov/dsta/coldstraw/controller/ModuleController.java
@@ -1,30 +1,44 @@
 package com.gov.dsta.coldstraw.controller;
 
+import com.gov.dsta.coldstraw.assembler.ModuleAssembler;
 import com.gov.dsta.coldstraw.model.Module;
 import com.gov.dsta.coldstraw.model.Notification;
 import com.gov.dsta.coldstraw.service.ModuleService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/modules")
 public class ModuleController {
 
     private final ModuleService moduleService;
+    private final ModuleAssembler moduleAssembler;
 
-    public ModuleController(ModuleService moduleService) {
+    public ModuleController(ModuleService moduleService, ModuleAssembler moduleAssembler) {
         this.moduleService = moduleService;
+        this.moduleAssembler = moduleAssembler;
     }
 
     @GetMapping()
-    public List<Module> getModules() {
-        return moduleService.getModules();
+    public CollectionModel<EntityModel<Module>> getModules() {
+        List<EntityModel<Module>> modules = moduleService
+                .getModules()
+                .stream()
+                .map(moduleAssembler::toModel)
+                .collect(Collectors.toList());
+        return moduleAssembler.toCollectionModel(modules);
     }
 
     @GetMapping("/{moduleId}")
-    public Module getModule(@PathVariable Long moduleId) {
-        return moduleService.getModule(moduleId);
+    public EntityModel<Module> getModule(@PathVariable Long moduleId) {
+        Module module = moduleService.getModule(moduleId);
+        return moduleAssembler.toModel(module);
     }
 
     @GetMapping("/{moduleId}/notifications")
@@ -33,8 +47,12 @@ public class ModuleController {
     }
 
     @PostMapping()
-    public Module createModule(@RequestBody Module module) {
-        return moduleService.addModule(module);
+    public ResponseEntity<Object> createModule(@RequestBody Module module) {
+        Module createdModule = moduleService.addModule(module);
+        EntityModel<Module> moduleModel = moduleAssembler.toModel(createdModule);
+        return ResponseEntity
+                .created(moduleModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .build();
     }
 
     @PutMapping("/{moduleId}")
@@ -44,7 +62,8 @@ public class ModuleController {
     }
 
     @DeleteMapping("/{moduleId}")
-    public void deleteModule(@PathVariable Long moduleId) {
+    public ResponseEntity<Void> deleteModule(@PathVariable Long moduleId) {
         moduleService.deleteModule(moduleId);
+        return ResponseEntity.noContent().build();
     }
 }
