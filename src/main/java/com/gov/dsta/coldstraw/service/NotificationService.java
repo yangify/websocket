8e5bd0ce65050e6902abe2e043a400561122bb58
hongyang-work <hong.yang.code@gmail.com>
2021-03-10 10:01:12 +0800

    removed redundant methods

--- src/main/java/com/gov/dsta/coldstraw/service/NotificationService.java
+++ src/main/java/com/gov/dsta/coldstraw/service/NotificationService.java
@@ -33,61 +33,6 @@ public class NotificationService {
         this.notificationReceiverRepository = notificationReceiverRepository;
     }
 
-    public List<Notification> getNotificationsByDateAndPage(Date startDate, Date endDate,
-                                                            Integer page, Integer size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return getNotificationsByDate(startDate, endDate, pageable);
-    }
-
-    public List<Notification> getNotificationsByPage(Integer page, Integer size) {
-        Pageable pageable = PageRequest.of(page, size);
-        return getNotifications(pageable);
-    }
-
-    public List<Notification> getNotificationsByDate(Date start, Date end, Pageable pageable) {
-        if (start == null && end == null) return getNotifications(pageable);
-        if (start == null) return getNotificationsBefore(end, pageable);
-        if (end == null) return getNotificationsAfter(start, pageable);
-        return getNotificationsBetween(start, end, pageable);
-    }
-
-    public List<Notification> getNotifications(Pageable pageable) {
-        if (pageable == null) return getNotifications();
-        Page<Notification> pageNotifications = notificationRepository.findAll(pageable);
-        return pageNotifications.getContent();
-    }
-
-    public List<Notification> getNotifications() {
-        return (List<Notification>) notificationRepository.findAll();
-    }
-
-    public List<Notification> getNotificationsBefore(Date end, Pageable pageable) {
-        if (pageable == null) return getNotificationsBefore(end);
-        return notificationRepository.findAllByDateBefore(end, pageable);
-    }
-
-    public List<Notification> getNotificationsBefore(Date end) {
-        return notificationRepository.findAllByDateBefore(end);
-    }
-
-    public List<Notification> getNotificationsAfter(Date start, Pageable pageable) {
-        if (pageable == null) return getNotificationsAfter(start);
-        return notificationRepository.findAllByDateAfter(start, pageable);
-    }
-
-    public List<Notification> getNotificationsAfter(Date start) {
-        return notificationRepository.findAllByDateAfter(start);
-    }
-
-    public List<Notification> getNotificationsBetween(Date start, Date end, Pageable pageable) {
-        if (pageable == null) return getNotificationsBetween(start, end);
-        return notificationRepository.findAllByDateBetween(start, end, pageable);
-    }
-
-    public List<Notification> getNotificationsBetween(Date start, Date end) {
-        return notificationRepository.findAllByDateBetween(start, end);
-    }
-
     public Notification getNotification(UUID notificationId) {
         return notificationRepository.findById(notificationId).orElseThrow(() -> new NotificationNotFoundException(notificationId));
     }
